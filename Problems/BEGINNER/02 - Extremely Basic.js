b89b993a67699fd14f948cb2fb4d8c59a979cbf5
William Thomas <william.arions@gmail.com>
2026-02-11 17:16:17 -0300

    feat:02 - Extremely Basic

---
+++ Problems/BEGINNER/02 - Extremely Basic.js
@@ -0,0 +1,27 @@
+/*Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+Input
+The input file will contain 2 integer numbers.
+
+Output
+Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+
+Obs.: don't forget the endline after all.
+
+Samples Input	
+10
+9
+Samples Output
+X = 19
+*/
+
+lines = ["10","9"]
+
+const [A, B] = lines.slice(",").map(item => parseInt(item))
+
+//console.log(A, B)
+
+const sum = A + B
+
+console.log(`X = ${sum}`)
+
