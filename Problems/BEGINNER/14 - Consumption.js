92164799085a19fc4a9282dcd42008bee0585c26
William Thomas <william.arions@gmail.com>
2026-02-15 10:29:47 -0300

    feat:14 - Consumption

---
+++ Problems/BEGINNER/14 - Consumption.js
@@ -0,0 +1,24 @@
+/*
+Calculate a car's average consumption being provided the total distance traveled (in Km) and the spent fuel total (in liters).
+
+Input
+The input file contains two values: one integer value X representing the total distance (in Km) and the second one is a floating point number Y  representing the spent fuel total, with a digit after the decimal point.
+
+Output
+Present a value that represents the average consumption of a car with 3 digits after the decimal point, followed by the message "km/l".
+
+Input Sample	
+500
+35.0
+Output Sample
+14.286 km/l
+*/
+
+lines = ["500", "35.0"]
+
+const distance = parseInt(lines[0])
+const spentFuel = parseFloat(lines[1])
+
+const consumptionAverage = distance / spentFuel
+
+console.log(`${consumptionAverage.toFixed(3)} km/l`)
