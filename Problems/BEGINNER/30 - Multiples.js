0d89d7bbc6b6c48b8e50bac81ffe68482d91f930
William Thomas <william.arions@gmail.com>
2026-02-21 10:00:26 -0300

    feat:30 - Multiples

---
+++ Problems/BEGINNER/30 - Multiples.js
@@ -0,0 +1,20 @@
+/*
+Read two integer values (A and B). After, the program should print the message "Sao Multiplos" (are multiples) or "Nao sao Multiplos" (aren’t multiples), corresponding to the read values.
+
+Input
+The input has two integer numbers.
+
+Output
+Print the relative message to the input as stated above.
+
+Input Sample	
+6 24
+Output Sample
+Sao Multiplos
+*/
+
+lines = ["6 24"]
+
+const [numberOne, numberTwo] = lines[0].split(" ").map(item => parseInt(item))
+
+console.log(numberOne % numberTwo === 0 || numberTwo % numberOne === 0 ? "Sao multiplos":"Nao sao multiplos")
